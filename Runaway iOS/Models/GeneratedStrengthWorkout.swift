ed1fb8ef02180497f05f4780531d0ec7a835c9c1
Jack Rudelic <jackrudelic@gmail.com>
2026-09-25 10:14:14 -0500

    fix: stabilize strength prescription fingerprints

--- Runaway iOS/Models/GeneratedStrengthWorkout.swift
+++ Runaway iOS/Models/GeneratedStrengthWorkout.swift
@@ -16,6 +16,45 @@ struct StrengthPrescriptionMetadata: Codable, Equatable, Sendable {
     let focusZones: Set<StrengthZone>
     let supportingZones: Set<StrengthZone>
     let exerciseIDs: [String]
+
+    private enum CodingKeys: String, CodingKey {
+        case policyVersion
+        case focusZones
+        case supportingZones
+        case exerciseIDs
+    }
+
+    init(
+        policyVersion: String,
+        focusZones: Set<StrengthZone>,
+        supportingZones: Set<StrengthZone>,
+        exerciseIDs: [String]
+    ) {
+        self.policyVersion = policyVersion
+        self.focusZones = focusZones
+        self.supportingZones = supportingZones
+        self.exerciseIDs = exerciseIDs
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        policyVersion = try container.decode(String.self, forKey: .policyVersion)
+        focusZones = Set(try container.decode([StrengthZone].self, forKey: .focusZones))
+        supportingZones = Set(try container.decode([StrengthZone].self, forKey: .supportingZones))
+        exerciseIDs = try container.decode([String].self, forKey: .exerciseIDs)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(policyVersion, forKey: .policyVersion)
+        try container.encode(Self.ordered(focusZones), forKey: .focusZones)
+        try container.encode(Self.ordered(supportingZones), forKey: .supportingZones)
+        try container.encode(exerciseIDs, forKey: .exerciseIDs)
+    }
+
+    private static func ordered(_ zones: Set<StrengthZone>) -> [StrengthZone] {
+        StrengthZone.allCases.filter(zones.contains)
+    }
 }
 
 struct GeneratedStrengthExercise: Identifiable, Equatable, Sendable {
